28393d3331fb7aeaa8552eb9c61c243e9b18ca3d
Tictac <paul.giraudet@gmail.com>
2018-12-13 14:16:56 +0100

    adding gitignore

---
+++ wp-content/themes/hestia/inc/customizer/controls/custom-controls/repeater/icon-picker/icons.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * The list of icons
+ *
+ * @package Hestia
+ */
+?>
+<div class="iconpicker-popover popover bottomLeft">
+	<div class="arrow"></div>
+	<div class="popover-title">
+		<input type="search" class="form-control iconpicker-search" placeholder="Type to filter">
+	</div>
+	<div class="popover-content">
+		<div class="iconpicker">
+			<div class="iconpicker-items">
+
+				<i data-type="iconpicker-item" title="behance" class="fab fa-behance"></i>
+				<i data-type="iconpicker-item" title="behance-square" class="fab fa-behance-square"></i>
+				<i data-type="iconpicker-item" title="facebook-f" class="fab fa-facebook-f"></i>
+				<i data-type="iconpicker-item" title="facebook-square" class="fab fa-facebook"></i>
+				<i data-type="iconpicker-item" title="google-plus-g" class="fab fa-google-plus-g"></i>
+				<i data-type="iconpicker-item" title="google-plus-square" class="fab fa-google-plus-square"></i>
+				<i data-type="iconpicker-item" title="linkedin-in" class="fab fa-linkedin-in"></i>
+				<i data-type="iconpicker-item" title="linkedin-square" class="fab fa-linkedin"></i>
+				<i data-type="iconpicker-item" title="twitter" class="fab fa-twitter"></i>
+				<i data-type="iconpicker-item" title="twitter-square" class="fab fa-twitter-square"></i>
+				<i data-type="iconpicker-item" title="vimeo-v" class="fab fa-vimeo-v"></i>
+				<i data-type="iconpicker-item" title="vimeo-square" class="fab fa-vimeo-square"></i>
+				<i data-type="iconpicker-item" title="youtube" class="fab fa-youtube"></i>
+				<i data-type="iconpicker-item" title="youtube-square" class="fab fa-youtube-square"></i>
+				<i data-type="iconpicker-item" title="ambulance" class="fas fa-ambulance"></i>
+				<i data-type="iconpicker-item" title="american-sign-language-interpreting" class="fas fa-american-sign-language-interpreting"></i>
+				<i data-type="iconpicker-item" title="anchor" class="fas fa-anchor"></i>
+				<i data-type="iconpicker-item" title="android" class="fab fa-android"></i>
+				<i data-type="iconpicker-item" title="apple" class="fab fa-apple"></i>
+				<i data-type="iconpicker-item" title="archive" class="fas fa-archive"></i>
+				<i data-type="iconpicker-item" title="chart-area" class="fas fa-chart-area"></i>
+				<i data-type="iconpicker-item" title="asterisk" class="fas fa-asterisk"></i>
+				<i data-type="iconpicker-item" title="car" class="fas fa-car"></i>
+				<i data-type="iconpicker-item" title="balance-scale" class="fas fa-balance-scale"></i>
+				<i data-type="iconpicker-item" title="ban" class="fas fa-ban"></i>
+				<i data-type="iconpicker-item" title="university" class="fas fa-university"></i>
+				<i data-type="iconpicker-item" title="bicycle" class="fas fa-bicycle"></i>
+				<i data-type="iconpicker-item" title="birthday-cake" class="fas fa-birthday-cake"></i>
+				<i data-type="iconpicker-item" title="btc" class="fab fa-btc"></i>
+				<i data-type="iconpicker-item" title="black-tie" class="fab fa-black-tie"></i>
+				<i data-type="iconpicker-item" title="bookmark" class="fas fa-bookmark"></i>
+				<i data-type="iconpicker-item" title="briefcase" class="fas fa-briefcase"></i>
+				<i data-type="iconpicker-item" title="bus" class="fas fa-bus"></i>
+				<i data-type="iconpicker-item" title="taxi" class="fas fa-taxi"></i>
+				<i data-type="iconpicker-item" title="camera" class="fas fa-camera"></i>
+				<i data-type="iconpicker-item" title="check" class="fas fa-check"></i>
+				<i data-type="iconpicker-item" title="child" class="fas fa-child"></i>
+				<i data-type="iconpicker-item" title="code" class="fas fa-code"></i>
+				<i data-type="iconpicker-item" title="coffee" class="fas fa-coffee"></i>
+				<i data-type="iconpicker-item" title="cog" class="fas fa-cog"></i>
+				<i data-type="iconpicker-item" title="comment-dots" class="fas fa-comment-dots"></i>
+				<i data-type="iconpicker-item" title="cube" class="fas fa-cube"></i>
+				<i data-type="iconpicker-item" title="dollar-sign" class="fas fa-dollar-sign"></i>
+				<i data-type="iconpicker-item" title="gem" class="far fa-gem"></i>
+				<i data-type="iconpicker-item" title="envelope" class="fas fa-envelope"></i>
+				<i data-type="iconpicker-item" title="female" class="fas fa-female"></i>
+				<i data-type="iconpicker-item" title="fire-extinguisher" class="fas fa-fire-extinguisher"></i>
+				<i data-type="iconpicker-item" title="glass-martini" class="fas fa-glass-martini"></i>
+				<i data-type="iconpicker-item" title="globe" class="fas fa-globe"></i>
+				<i data-type="iconpicker-item" title="graduation-cap" class="fas fa-graduation-cap"></i>
+				<i data-type="iconpicker-item" title="heartbeat" class="fas fa-heartbeat"></i>
+				<i data-type="iconpicker-item" title="heart" class="fas fa-heart"></i>
+				<i data-type="iconpicker-item" title="bed" class="fas fa-bed"></i>
+				<i data-type="iconpicker-item" title="hourglass" class="fas fa-hourglass"></i>
+				<i data-type="iconpicker-item" title="home" class="fas fa-home"></i>
+				<i data-type="iconpicker-item" title="gavel" class="fas fa-gavel"></i>
+				<i data-type="iconpicker-item" title="lock" class="fas fa-lock"></i>
+				<i data-type="iconpicker-item" title="unlock" class="fas fa-unlock"></i>
+				<i data-type="iconpicker-item" title="map-signs" class="fas fa-map-signs"></i>
+				<i data-type="iconpicker-item" title="paint-brush" class="fas fa-paint-brush"></i>
+				<i data-type="iconpicker-item" title="plane" class="fas fa-plane"></i>
+				<i data-type="iconpicker-item" title="rocket" class="fas fa-rocket"></i>
+				<i data-type="iconpicker-item" title="puzzle-piece" class="fas fa-puzzle-piece"></i>
+				<i data-type="iconpicker-item" title="shield-alt" class="fas fa-shield-alt"></i>
+				<i data-type="iconpicker-item" title="tag" class="fas fa-tag"></i>
+				<i data-type="iconpicker-item" title="times" class="fas fa-times"></i>
+				<i data-type="iconpicker-item" title="user" class="fas fa-user"></i>
+				<i data-type="iconpicker-item" title="user-tie" class="fas fa-user-tie"></i>
+				<i data-type="iconpicker-item" title="user-md" class="fas fa-user-md"></i>
+				<i data-type="iconpicker-item" title="video" class="fas fa-video"></i>
+				<i data-type="iconpicker-item" title="wordpress" class="fab fa-wordpress"></i>
+				<i data-type="iconpicker-item" title="gift" class="fas fa-gift"></i>
+
+			</div> <!-- /.iconpicker-items -->
+		</div> <!-- /.iconpicker -->
+	</div> <!-- /.popover-content -->
+</div> <!-- /.iconpicker-popover -->
